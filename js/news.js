77ee790da70fdc704f17d871a0d580a079927506
AshrafulMorsalinSizan99 <jucse28.377@gmail.com>
2022-09-03 16:10:44 +0600

    load completed

--- js/news.js
+++ js/news.js
@@ -40,46 +40,46 @@ const loadNews = async (categoryId) => {
     // console.log(url);
     const res = await fetch(url);
     const data = await res.json();
-    displayNewsDetails(data.data[0]);
+    displayNewsDetails(data.data);
     // console.log(data.data[0]);
 
 }
 const displayNewsDetails = news => {
     const newsContainer = document.getElementById('news-container');
-    newsContainer.innerHTML = ``;
-    // newsContainer.textContent = '';
-    const newsDiv = document.createElement('div');
-    newsDiv.classList.add('card');
-    newsDiv.innerHTML = `
-           <figure><img src="${news.image_url}" alt="Album"></figure>
-           <div class="card-body">
-                        <h2 class="card-title">${news.title}</h2>
-                        <p>${news.details.length > 250 ? news.details.slice(0, 250) + '...' : news.details}</p>
-                        <img src="${news.author.img}">
-                        <p>${news.author.name}</p>
-                        <p>Total View: ${news.total_view}</p>
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Show Details</button>
-                        </div>
-           </div>
-    `;
-    newsContainer.appendChild(newsDiv);
-    // news.forEach(elem => {
-    //     const newsDiv = document.createElement('div');
-    //     newsDiv.classList.add('card');
-    //     newsDiv.innerHTML = `
-    //        <figure><img src="${elem.image_url}" alt="Album"></figure>
+    // newsContainer.innerHTML = ``;
+    newsContainer.textContent = '';
+    // const newsDiv = document.createElement('div');
+    // newsDiv.classList.add('card');
+    // newsDiv.innerHTML = `
+    //        <figure><img src="${news.image_url}" alt="Album"></figure>
     //        <div class="card-body">
-    //                     <h2 class="card-title">${elem.title}</h2>
-    //                     <p>${elem.details.length > 250 ? elem.details.slice(0, 250) + '...' : elem.details}</p>
-    //                     <img src="${elem.author.img}">
-    //                     <p>${elem.author.name}</p>
-    //                     <p>Total View: ${elem.total_view}</p>
+    //                     <h2 class="card-title">${news.title}</h2>
+    //                     <p>${news.details.length > 250 ? news.details.slice(0, 250) + '...' : news.details}</p>
+    //                     <img src="${news.author.img}">
+    //                     <p>${news.author.name}</p>
+    //                     <p>Total View: ${news.total_view}</p>
     //                     <div class="card-actions justify-end">
     //                         <button class="btn btn-primary">Show Details</button>
     //                     </div>
     //        </div>
     // `;
-    //     newsContainer.appendChild(newsDiv);
-    // });
+    // newsContainer.appendChild(newsDiv);
+    news.forEach(elem => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('card');
+        newsDiv.innerHTML = `
+               <figure><img src="${elem.image_url}" alt="Album"></figure>
+               <div class="card-body">
+                            <h2 class="card-title">${elem.title}</h2>
+                            <p>${elem.details.length > 250 ? elem.details.slice(0, 250) + '...' : elem.details}</p>
+                            <img src="${elem.author.img}">
+                            <p>${elem.author.name}</p>
+                            <p>Total View: ${elem.total_view}</p>
+                            <div class="card-actions justify-end">
+                                <button class="btn btn-primary">Show Details</button>
+                            </div>
+               </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    });
 }
